c460c927fb4d897436ec58e72e7c02c1e3011011
bettyje <bettyjelagat156@gmail.com>
2024-11-20 15:35:52 +0300

    updating files

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
 
+  return (
+    <div>
+      <input type="password" onChange={handleChange} placeholder="Password" />
+    </div>
+  );
+};
 export default Keypad;
